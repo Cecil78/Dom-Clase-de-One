4d979ee946d736ac584aade13598a1963f6a0e58
Michelle <cecilabreu24@hotmail.com>
2023-05-04 15:40:04 -0400

    clase02

--- 1821-manipulando-dom-con-js-proyecto_inicial/script.js
+++ 1821-manipulando-dom-con-js-proyecto_inicial/script.js
@@ -1,9 +1,10 @@
 const btn=document.querySelector("[data-form-btn]");
 
-console.log(btn);
+
 
 btn.addEventListener("click", function (){
     console.log("crear tarea");
+    console.log(btn);
 });
 
 
