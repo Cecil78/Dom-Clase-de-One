822a7f7ecb21ff11d56052c961f06f2624f89d37
Michelle <cecilabreu24@hotmail.com>
2023-05-04 15:59:57 -0400

    hola

--- 1821-manipulando-dom-con-js-proyecto_inicial/script.js
+++ 1821-manipulando-dom-con-js-proyecto_inicial/script.js
@@ -1,11 +1,11 @@
 const btn=document.querySelector("[data-form-btn]");
 
 console.log(btn);
-console.log(input.value);
+
 
 btn.addEventListener("click", function (){
     const input= document.querySelector("[data-form-input]");
-    console.log(btn);
+    console.log(input.value);
 });
 
 
